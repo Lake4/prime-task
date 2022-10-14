6c79a627eb9de46232bc4de7f78b32651d28e197
Lake4 <Anton@LAPTOP-KTMMLBSI>
2022-10-14 20:35:46 +0300

    improved structure based on feedback and fixed a mistake with datatypes used in main

--- primeTask.c
+++ primeTask.c
@@ -1,12 +1,12 @@
-#include "primeTask.h"
+#include "primes.h"
 
 int main() {
 
     //give 10 and 11 digit numbers for starting points of finding prime numbers
-    long long first10digPrime = findPrime(1000000000);
-    long long first11digPrime = findPrime(10000000000);
+    unsigned long long first10digPrime = findPrime(1000000000);
+    unsigned long long first11digPrime = findPrime(10000000000);
 
-    long long product = first10digPrime*first11digPrime;
+    unsigned long long product = first10digPrime*first11digPrime;
     print15Digits(product);
 
     return 0;
---
+++ primes.h
@@ -0,0 +1,8 @@
+#ifndef PRIMES_H
+#define PRIMES_H
+
+int isPrime(unsigned long long number);
+unsigned long long findPrime(unsigned long long start);
+void print15Digits(unsigned long long number);
+
+#endif
